d00b76aacb288180cf5a5948abbe076c0c496673
JESUS <marcos.jesus.py25@gmail.com>
2025-08-22 14:30:56 -0300

    Update vai_corinthians.py

--- vai_corinthians.py
+++ vai_corinthians.py
@@ -2,8 +2,8 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def perfil():
+@app.route("index")
+def index():
     return render_template("index.html")
 
 @app.route("/home")
@@ -16,4 +16,5 @@ def sair():
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
